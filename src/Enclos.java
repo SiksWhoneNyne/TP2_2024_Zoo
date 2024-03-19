78a552a7eeecdbbfb2bc48971247a5602080f869
Wenceslas HOTOEUA <wenceslas.hotoeua@gmail.com>
2024-03-19 10:37:15 -0400

    2eme commit: Debut code Enclos.java

--- src/Enclos.java
+++ src/Enclos.java
@@ -1,2 +1,19 @@
 public class Enclos {
+    private String nom;
+    private int nombreAnimaux;
+    private int capaciteMax;
+    private Animal [] animaux;
+    private Veterinaire veterinaire;
+    private Gardien gardien;
+
+    public Enclos (String nom, int capaciteMax, Veterinaire veterinaire, Gardien gardien) {
+        this.nom = nom;
+        this.capaciteMax = capaciteMax;
+        this.veterinaire = veterinaire;
+        this.gardien = gardien;
+        this.animaux = new Animal[capaciteMax];
+        this.nombreAnimaux = 0;
+    }
+
+    // Debut Methodes =======================================================
 }
